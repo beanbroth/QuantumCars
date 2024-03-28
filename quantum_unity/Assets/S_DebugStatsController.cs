0949a6326248d3fa18a76fbe08731a4451f89e86
beanbroth <dashiell.corning@mymail.champlain.edu>
2024-03-28 16:14:22 +0100

    0.0.19
    Added FPS and version display to UI

--- quantum_unity/Assets/S_DebugStatsController.cs
+++ quantum_unity/Assets/S_DebugStatsController.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using TMPro;
 using UnityEngine;
 
 public class S_DebugStatsController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI versionText;
+    public TextMeshProUGUI fpsText; // UI Text for displaying the FPS
+
+    private float deltaTime = 0.0f;
+
     void Start()
     {
-        
+        string version = Resources.Load<TextAsset>("version").text.Trim();
+        versionText.text = "Version " + version;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Calculate deltaTime
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+
+        // Calculate FPS
+        float fps = 1.0f / deltaTime;
+
+        // Update FPS Text
+        fpsText.text = string.Format("FPS: {0:0.}", fps);
     }
 }
